42625929cedc4bd32d85c95fc2a2ac639b2bf9f7
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-27 14:29:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './css/styles.css';
 
 const formEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    
+    const searchQuery = event.currentTarget.elements.searchQuery.value;
+
+  const searchParams = new URLSearchParams({
+      key: '34753059-f7902d1f02de9c533025c1a5e',
+      q: `${searchQuery}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+  });
+
+    const url = `https://pixabay.com/api/?${searchParams}`;
+    console.log(url);
+    
+    fetch(url).then(response => response.json()).then(console.log)
+}
