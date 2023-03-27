7cfc39a09570c23dec231de88796066dc5aad208
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-27 18:00:08 +0300

    Update searchImages-api.js

--- src/searchImages-api.js
+++ src/searchImages-api.js
@@ -1,13 +1,15 @@
 export default class ImagesApiService {
-  constructor() {}
+  constructor() {
+    this.searchQuery = '';
+  }
 
-  fetchImages(searchQuery) {
+  fetchImages() {
     const BASE_URL = 'https://pixabay.com/api/';
     const API_KEY = '34753059-f7902d1f02de9c533025c1a5e';
 
     const searchParams = new URLSearchParams({
       key: `${API_KEY}`,
-      q: `${searchQuery}`,
+      q: `${this.searchQuery}`,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: 'true',
@@ -26,5 +28,13 @@ export default class ImagesApiService {
         return response.json();
       })
       .then(console.log);
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
